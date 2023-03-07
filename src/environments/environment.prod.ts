d1bf8edd07a9ff3d26714ad0a038a486cc64286a
Juan Alesandri <juan.alesandri.acc@gmail.com>
2023-03-07 12:05:17 -0300

    Se incluye environment variable

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    //firebaseAPIKey: 'YOUR_FIREBASE_API_KEY',
+    url_base: 'https://backend-recipes-bootcamps-tribe-production.up.railway.app/'
+};
